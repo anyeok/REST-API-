fe5feef44ac6f717a12edc127cdf2ed6ca8126b7
anyeok <https://github.com/anyeok>
2024-11-05 13:26:55 +0900

    시작

---
+++ src/test/java/com/example/REST_API_TEST/RestApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.REST_API_TEST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
